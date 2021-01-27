450734605dac7ccee99ab118988759c7c0fa0726
Rocik764 <63047043+Rocik764@users.noreply.github.com>
2021-01-27 20:27:36 +0100

    remade modal dialog in orders list in admins view - now it'll display table with details inside model dialog. Same with logged-in user's orders details on his profile.

--- src/main/java/local/umg/susersmvc/restController/OrdersDetailsRestController.java
+++ src/main/java/local/umg/susersmvc/restController/OrdersDetailsRestController.java
@@ -1,10 +1,17 @@
 package local.umg.susersmvc.restController;
 
+import local.umg.susersmvc.details.CustomUserDetails;
 import local.umg.susersmvc.model.OrderDetails;
+import local.umg.susersmvc.model.User;
 import local.umg.susersmvc.service.OrdersDetailsService;
+import local.umg.susersmvc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.sql.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,15 +23,39 @@ public class OrdersDetailsRestController {
     @Autowired
     private OrdersDetailsService ordersDetailsService;
 
+    @Autowired
+    private UserService userService;
+
     /**
      * Method used in AJAX to show modal dialog with detailed order's information
      */
     @GetMapping(value = "/showOrdersDetails/{uId}/{date}")
-    public String addProductToCart(@PathVariable(name = "uId") Long uId, @PathVariable(name = "date") Date date) {
-        System.out.println("showOrdersDetailsListRest");
-        List<OrderDetails> orderDetails = ordersDetailsService.findAllByUserIdAndDate(uId, date);
-        String listString = orderDetails.stream().map(Object::toString)
-                .collect(Collectors.joining(""));
-        return listString;
+    public List<OrderDetails> showOrdersDetails(@PathVariable(name = "uId") Long uId, @PathVariable(name = "date") Date date) {
+        System.out.println("showOrdersDetails");
+        try {
+            List<OrderDetails> orderDetails = ordersDetailsService.findAllByUserIdAndDate(uId, date);
+            return orderDetails;
+        } catch (EntityNotFoundException e) {
+            return null;
+        }
+    }
+
+    @GetMapping(value = "/showOrdersDetailsUser/{date}")
+    public List<OrderDetails> showOrdersDetailsUser(@AuthenticationPrincipal CustomUserDetails loggedUser, @PathVariable(name = "date") Date date) {
+        System.out.println("showOrdersDetailsUser");
+        User uId = getUser(loggedUser);
+        try {
+            List<OrderDetails> orderDetails = ordersDetailsService.findAllByUserIdAndDate(uId.getId(), date);
+            return orderDetails;
+        } catch (EntityNotFoundException e) {
+            return null;
+        }
+    }
+
+    private User getUser(CustomUserDetails loggedUser) {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(loggedUser, loggedUser.getPassword(), loggedUser.getAuthorities());
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long id = userDetails.getId();
+        return userService.get(id);
     }
 }
